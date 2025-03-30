9a5566026b7226b1e58b8aca44f28df694bc9688
Minoru Sakamoto <p2020640@gmail.com>
2025-03-30 23:14:55 +0900

    Update SHA256 value in Homebrew formula

--- cmdpanic.rb
+++ cmdpanic.rb
@@ -2,7 +2,7 @@ class Cmdpanic < Formula
   desc "A command line whack-a-mole game"
   homepage "https://github.com/sakamotodesu/cmdpanic"
   url "https://github.com/sakamotodesu/cmdpanic/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "YOUR_SHA256_HERE" # リリース時に更新する必要があります
+  sha256 "75d3fd5dbec90c86b65589c232f2ab9b30619da65b7e92126082acf462bd456eE"
 
   depends_on "rust" => :build
 
